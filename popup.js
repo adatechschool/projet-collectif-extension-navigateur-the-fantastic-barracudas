01edec4452d7563dc95f448d3787fd900624a85b
magueloneguinaud <146751391+magueloneguinaud@users.noreply.github.com>
2024-01-11 16:57:41 +0100

    ajout et fonction du toggle

--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
+//chrome.storage.local.set({ "etatCheckbox": false })
+console.log(document.getElementById("toggle").checked)
+document.getElementById("toggle").addEventListener("change", async function () {
+    if (this.checked) {
+        //chrome.storage.local.get("etatCheckbox", function (data) {
+        //let petitBoolean = data.etatCheckbox;
+        let petitBoolean = true;
+        chrome.storage.local.set({ "etatCheckbox": petitBoolean });
+        console.log("true");
+        let p = await chrome.storage.local.get(etatCheckbox);
+        console.log(p)
+        //});
+    } else {
+        //chrome.storage.local.get("etatCheckbox", function (data) {
+        //let petitBoolean = data.etatCheckbox;
+        let petitBoolean = false;
+        chrome.storage.local.set({ "etatCheckbox": petitBoolean });
+        console.log("false");
+        let t = await chrome.storage.local.get(etatCheckbox);
+        console.log(t)
+        // })
+    }
+})
+
+
 //Récuperer l'url des images que l'on a stocké dans le background.
 document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.get({ images: [] }, function (data) {
