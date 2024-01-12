ad6305ae2d2664f29bdb30167649808f70fe4358
Alexis Moulliere <alexismoulliere@MacBook-Pro-de-Alexis.local>
2024-01-12 16:49:33 +0100

    selection text, mvp atteint

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
 //Récuperer le message du fichier content et le stocker sur le storage local.
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  chrome.storage.local.get({ images: [] }, function (data) {
-    let image = data.images;
-    image.push(message.imageUrl);
-    chrome.storage.local.set({ images: image });
-    console.log(image);
-  });
+  console.log("gfrgrgrg", message.imageUrl);
+  if (message.imageUrl) {
+    chrome.storage.local.get({ images: [] }, function (data) {
+      let image = data.images;
+      image.push(message.imageUrl);
+      chrome.storage.local.set({ images: image });
+      console.log(image);
+    });
+  }
+
+  if (message.text_content) {
+    chrome.storage.local.get({ tab_textes: [] }, function (data) {
+      let text = data.tab_textes;
+      text.push(message.text_content);
+      chrome.storage.local.set({ tab_textes: text });
+      console.log(text);
+    });
+  }
 });
 
 // const contextMenuItem = {
--- content.js
+++ content.js
@@ -6,12 +6,23 @@ function selectUrl(e) {
   }
 }
 
+function selectText() {
+  const selectedText = window.getSelection().toString();
+  console.log(selectText);
+  if (selectedText.rangeCount > 0 || selectedText !== "\n") {
+    console.log(selectedText);
+    chrome.runtime.sendMessage({ text_content: selectedText });
+  }
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log(request);
   if (request.toggleEtat) {
     document.addEventListener("click", selectUrl);
+    document.addEventListener("mouseup", selectText);
   } else {
     console.log("hey");
     document.removeEventListener("click", selectUrl);
+    document.removeEventListener("mouseup", selectText);
   }
 });
